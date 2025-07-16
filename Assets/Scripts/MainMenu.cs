74d5eae0cdf3c0671157dabb2718d5e51f9fe9b1
Sarra <sarrakoussaierr@gmail.com>
2025-07-16 12:34:42 +0200

    menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,108 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Global Language
+public static class GlobalLanguage
+{
+    public static string language = "english";
+}
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Audio")]
+    public AudioMixer mainAudioMixer;
+    public AudioMixer musicAudioMixer;
+
+    [Header("Game Settings")]
+    public string gameSceneName = "Main";
+
+    [Header("UI Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject settingsPanel;
+
+    // Language dropdown function
+    public void SetLanguage(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                GlobalLanguage.language = "english";
+                break;
+            case 1:
+                GlobalLanguage.language = "french";
+                break;
+            default:
+                GlobalLanguage.language = "english";
+                break;
+        }
+
+        PlayerPrefs.SetString("Language", GlobalLanguage.language);
+        PlayerPrefs.Save();
+    }
+
+    // Quality dropdown function
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+        PlayerPrefs.SetInt("Quality", qualityIndex);
+        PlayerPrefs.Save();
+    }
+
+    // Overall volume slider function
+    public void SetOverallVolume(float volume)
+    {
+        float dbValue = volume > 0 ? Mathf.Log10(volume) * 20f : -80f;
+
+        if (mainAudioMixer != null)
+        {
+            mainAudioMixer.SetFloat("Volume", dbValue);
+        }
+
+        PlayerPrefs.SetFloat("OverallVolume", volume);
+        PlayerPrefs.Save();
+    }
+
+    // Music volume slider function
+    public void SetMusicVolume(float volume)
+    {
+        float dbValue = volume > 0 ? Mathf.Log10(volume) * 20f : -80f;
+
+        if (musicAudioMixer != null)
+        {
+            musicAudioMixer.SetFloat("Volume", dbValue);
+        }
+
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+        PlayerPrefs.Save();
+    }
+
+    // Settings button function
+    public void Settings()
+    {
+        // Add your settings panel logic here
+        Debug.Log("Settings opened");
+    }
+
+    // Play button function
+    public void Play()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    // Exit button function
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void Back()
+    {
+        settingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+}
